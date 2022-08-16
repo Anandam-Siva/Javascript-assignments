fdd570918df91563814bcfbfc0461e24f4f2334e
Anandam Siva <sivadevops80123@gmail.com>
2022-08-17 00:42:52 +0530

    Javascript assignments- section A

--- problem2.js
+++ problem2.js
@@ -4,12 +4,12 @@ var c;
 var d;
 c = ++b;
 d = a++;
-// c++;
-// b++;
-// ++a;
-// console.log(a + ' ' + b + ' ' + c);
-// console.log(c);
-// console.log(a)
-// console.log(d);
+c++;
+b++;
+++a;
+console.log(a + ' ' + b + ' ' + c);
+console.log(c);
+console.log(a)
+console.log(d);
 
 
